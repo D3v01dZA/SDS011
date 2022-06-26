449dedcba280b28ca9cecf71c42fde562ff869e5
Christopher Altona <caltona1@gmail.com>
2022-06-26 11:43:55 -0400

    Push to MQTT

--- run_sds011s.py
+++ run_sds011s.py
@@ -22,7 +22,7 @@ if __name__ ==  '__main__':
     usbs = get_usb()
     processs = list()
     for port in usbs:
-        p = SDS011(port=port, push_mqtt=False, interval=os.environ['SDS_INTERVAL'])
+        p = SDS011(port=port, push_mqtt=True, interval=os.environ['SDS_INTERVAL'])
         processs.append(p)
         
     while True:
